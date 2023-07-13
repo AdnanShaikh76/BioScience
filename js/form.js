a2e0d3782e68b73afad87081ea2fa8db83101228
Adnan Shaikh <105547616+AdnanShaikh76@users.noreply.github.com>
2023-07-13 13:12:17 +0530

    Adding Updated Files

---
+++ js/form.js
@@ -0,0 +1,5 @@
+const contactus = document.getElementById("contactus");
+const form = document.getElementsByClassName("form")
+    contactus.onclick = () =>{
+        form.style.display = 'block';
+    }
