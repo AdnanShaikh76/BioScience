60d5f45cb63bf4bc091e93fc7543209329562b9f
Adnan Shaikh <105547616+AdnanShaikh76@users.noreply.github.com>
2023-11-21 23:24:39 +0530

    Delete js directory

--- js/form.js
+++
@@ -1,5 +0,0 @@
-const contactus = document.getElementById("contactus");
-const form = document.getElementsByClassName("form")
-    contactus.onclick = () =>{
-        form.style.display = 'block';
-    }
